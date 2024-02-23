1fe67fb1c3f02353d80db73dd5a33f74031ab73d
Pierre Wensel <pierrewensel@gmail.com>
2024-02-22 20:53:11 -0500

    Motif branch change to found statement to test conflict

--- seqClass.py
+++ seqClass.py
@@ -25,6 +25,6 @@ if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
-        print("FOUND")
+        print("TESTING MOTIF BRANCH CONFLICT FOUND")
     else:
         print("TESTING NOT FOUND")
