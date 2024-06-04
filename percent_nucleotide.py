7e6b65fc2454292ee0bafde3ffbe1e4cda0fc929
pwensel <pierrewensel@gmail.com>
2024-06-04 07:41:30 -0400

    Create percent_nucleotide.py
    Created .py file that will be pulled into my local repository for collaboration and edits without downloading or cloning others' repositories.

---
+++ percent_nucleotide.py
@@ -0,0 +1,2 @@
+#This is a new .py file that I created in my remote Github Collaboration_Percent_Nucleotide branch that I will pull from my local repository to collaborate and edit a script that 
+#will calculate % nucleaotide in a user-entered sequence. This is done to avoid having to clone repositories of others.
